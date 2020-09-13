7c27fd6a8b87b3cbd96188618594b9257411cc86
Lukas Biermann <lukas.biermann@semalytix.de>
2020-09-13 09:25:44 +0200

    adapted docker script

--- src/HDT/prepare_data_day_ic.sh
+++ src/HDT/prepare_data_day_ic.sh
@@ -1,6 +1,9 @@
 mkdir -p $(pwd)/bear-b/day/ic/nt/
 mkdir -p $(pwd)/bear-b/day/ic/hdt/
+mkdir -p $(pwd)/bear-b/day/ic/output/
+mkdir -p $(pwd)/bear-b/day/ic/queries/
 (cd $(pwd)/bear-b/day/ic/nt/ && wget https://aic.ai.wu.ac.at/qadlod/bear/BEAR_B/datasets/day/IC/alldata.IC.nt.tar.gz && tar xf alldata.IC.nt.tar.gz && rm alldata.IC.nt.tar.gz && gzip -d *.nt.gz )
+(cd $(pwd)/bear-b/day/ic/queries/ && wget https://aic.ai.wu.ac.at/qadlod/bear/BEAR_B/Queries/p/p.txt && wget https://aic.ai.wu.ac.at/qadlod/bear/BEAR_B/Queries/po/po.txt)
 for f in $(pwd)/bear-b/day/ic/nt/*.nt
 do
 	input="$(pwd)/bear-b/day/ic/nt/$(basename $f)"
--- src/HDT/run-docker.sh
+++ src/HDT/run-docker.sh
@@ -2,12 +2,9 @@
 
 policies="ic"
 categories="mat diff ver"
-queries="s-queries-lowCardinality.txt s-queries-highCardinality.txt p-queries-highCardinality.txt p-queries-lowCardinality.txt o-queries-highCardinality.txt o-queries-lowCardinality.txt po-queries-highCardinality.txt po-queries-lowCardinality.txt so-queries-lowCardinality.txt sp-queries-highCardinality.txt sp-queries-lowCardinality.txt spo-queries.txt"
-#queries="p.txt po.txt"
-
-# TODO
+#queries="s-queries-lowCardinality.txt s-queries-highCardinality.txt p-queries-highCardinality.txt p-queries-lowCardinality.txt o-queries-highCardinality.txt o-queries-lowCardinality.txt po-queries-highCardinality.txt po-queries-lowCardinality.txt so-queries-lowCardinality.txt sp-queries-highCardinality.txt sp-queries-lowCardinality.txt spo-queries.txt"
+queries="p.txt po.txt"
 categories="mat"
-queries=$(cd /mnt/datastore/data/dslab/experimental/patch/BEAR/queries_bearb/ && ls -v)
 
 for policy in ${policies[@]}; do
 for category in ${categories[@]}; do
@@ -17,9 +14,9 @@ docker run -it --rm \
     -e POLICY="$policy" \
     -e CATEGORY="$category" \
     -e QUERY="$query" \
-    -v /mnt/datastore/data/dslab/experimental/patch/bearb-day-hdt/:/var/data/dataset/ \
-    -v /mnt/datastore/data/dslab/experimental/patch/BEAR/queries_bearb/:/var/data/queries/ \
-    -v /mnt/datastore/data/dslab/experimental/patch/output/:/var/data/output/ \
+    -v $(pwd)/bear-b/day/ic/hdt/:/var/data/dataset/ \
+    -v $(pwd)/bear-b/day/ic/queries:/var/data/queries/ \
+    -v $(pwd)/bear-b/day/ic/output/:/var/data/output/ \
     bear-hdt
 
 done
